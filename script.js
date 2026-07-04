a2d5689db1c5e8eb91972e489a80c75308099008
Uday <bhupelliuday@gmail.com>
2026-07-04 17:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student {
+	name : "John",
+	age : "24",
+	city : "Hyderabad",
+};
+
+function getkeys(obj){
+	return obj.keys(obj);
+}
+console.log(getkeys(student));
